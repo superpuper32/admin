96533e073ff6301e9d3ef45660e7d4837da5c2a2
superpuper32 <pochitalin_m@mail.ru>
2020-10-11 23:04:10 +0300

    auth logic added

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdSIc3OOI-Kurnton5S3OaUvyA8USUF4o",
+  authDomain: "fir-app-9b96e.firebaseapp.com",
+  databaseURL: "https://fir-app-9b96e.firebaseio.com",
+  projectId: "fir-app-9b96e",
+  storageBucket: "fir-app-9b96e.appspot.com",
+  messagingSenderId: "876210419848",
+  appId: "1:876210419848:web:4ceaaa75bf84e66755473d",
+  measurementId: "G-8MCBFGEGCZ",
+};
+
+export const appName = "fir";
